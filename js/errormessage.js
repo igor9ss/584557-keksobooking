4dc9fd749c1d33b8e0444098ca63eff78c4a8acc
Igor Zorin <zorin.igor.jaroslavovich@gmail.com>
2018-04-29 21:38:14 +0300

    вынес переменную errorPopupElement и описание стилей из фун-ии window.errorMessage.show

--- js/errormessage.js
+++ js/errormessage.js
@@ -1,22 +1,23 @@
 'use strict';
 
 (function () {
+
+  var errorPopupElement = document.createElement('div');
+  errorPopupElement.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red; color: white;';
+  errorPopupElement.classList.add('errorPopup');
+  errorPopupElement.style.position = 'absolute';
+  errorPopupElement.style.left = 0;
+  errorPopupElement.style.right = 0;
+  errorPopupElement.style.fontSize = '45px';
+  document.body.insertAdjacentElement('afterbegin', errorPopupElement);
+
   var closeErrorMessage = function () {
     document.querySelector('.errorPopup').outerHTML = '';
   };
 
   window.errorMessage = {
     'show': function (errorMessage) {
-      var errorPopupElement = document.createElement('div');
-      errorPopupElement.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red; color: white;';
-      errorPopupElement.classList.add('errorPopup');
-      errorPopupElement.style.position = 'absolute';
-      errorPopupElement.style.left = 0;
-      errorPopupElement.style.right = 0;
-      errorPopupElement.style.fontSize = '45px';
-
       errorPopupElement.textContent = errorMessage;
-      document.body.insertAdjacentElement('afterbegin', errorPopupElement);
 
       setTimeout(closeErrorMessage, 10000);
     }
