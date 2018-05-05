dea7d6e1f86753b75f685811981cc9bec9977ebc
Igor Zorin <zorin.igor.jaroslavovich@gmail.com>
2018-05-05 15:35:45 +0300

    фильтры на select'ах

--- js/filter.js
+++ js/filter.js
@@ -2,22 +2,28 @@
 
 (function () {
   var filterTypeSelect = document.querySelector('#housing-type');
+  var filterPriceSelect = document.querySelector('#housing-price');
+  var filterRoomsSelect = document.querySelector('#housing-rooms');
+  var filterGuestsSelect = document.querySelector('#housing-guests');
 
   window.filter = {
     filterPins: function (pins) {
-
       return pins
           .filter(function (pin) {
             return filterTypeSelect.value === 'any' || pin.offer.type === filterTypeSelect.value;
           })
-          .filter(function () {
-            return true;
+          .filter(function (pin) {
+            return filterPriceSelect.value === 'any' ||
+            ((filterPriceSelect.value === 'low') && (pin.offer.price <= 10000)) ||
+            ((filterPriceSelect.value === 'middle') && (pin.offer.price >= 10000) && (pin.offer.price <= 50000)) ||
+            ((filterPriceSelect.value === 'high') && (pin.offer.price >= 50000));
           })
-          .filter(function () {
-            return true;
+          .filter(function (pin) {
+            return filterRoomsSelect === 'any' || pin.offer.rooms === +filterRoomsSelect.value;
           })
-          .filter(function () {
-            return true;
+
+          .filter(function (pin) {
+            return filterGuestsSelect === 'any' || pin.offer.guests >= +filterGuestsSelect.value;
           });
     }
   };
